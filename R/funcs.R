2a5eccd340ae8399d2fc0366eaeecab15461b7ed
fawda123 <mbafs2012@gmail.com>
2016-05-26 18:20:02 -0500

    cpue now as weight per effort, data only

--- R/funcs.R
+++ R/funcs.R
@@ -4,7 +4,8 @@
 # dat_in fish_all dataset created in dat_proc.r
 # bhd_yoy upper length limit (mm) for bullhead yoy
 # cap_yoy upper length limit (mm) for carp yoy
-cpue_fun <- function(dat_in, bhd_yoy = 100, cap_yoy = 150){
+# bywt logical indicating if CPUE is biomass/effort if TRUE, otherwise count/effort
+cpue_fun <- function(dat_in, bhd_yoy = 100, cap_yoy = 150, bywt = TRUE){
   
   library(tidyr)
   library(dplyr)
@@ -16,20 +17,46 @@ cpue_fun <- function(dat_in, bhd_yoy = 100, cap_yoy = 150){
   dat$age[dat$sp_abb == 'CAP' & dat$tl_mm < cap_yoy] <- 'yoy'
   dat$age[dat$sp_abb == 'BHD' & dat$tl_mm < bhd_yoy] <- 'yoy'
   
-  # summarize by cpue
-  dat <- group_by(dat, dow, date, type, sp_abb, age, effort) %>% 
-    summarise(
-      n = length(tl_mm)
-    ) %>% 
-    ungroup %>% 
-    mutate(cpue = as.numeric(n)/as.numeric(effort)) %>% 
+  # add weight column 
+  # coeffs from Handbook of Freshwater Fishery Biology
+  dat$wt_g <- NA
+  dat[dat$sp_abb %in% 'CAP', 'wt_g'] <- 10^(-4.44245 + 2.83840 * log10(dat[dat$sp_abb %in% 'CAP', 'tl_mm']))
+  dat[dat$sp_abb %in% 'BHD', 'wt_g'] <- 10^(-4.60512 + 2.88495 * log10(dat[dat$sp_abb %in% 'BHD', 'tl_mm']))
+  
+  # wt to kg
+  dat <- rename(dat, wt_kg = wt_g) %>% 
+    mutate(wt_kg = wt_kg * 0.001)
+  
+  # sum weights for species if T  
+  if(bywt){
+
+    dat <- group_by(dat, dow, date, type, sp_abb, age, effort) %>% 
+      summarise(
+        ctch = sum(wt_kg, na.rm = TRUE)
+      ) %>% 
+      ungroup
+    
+  # otherwise get counts  
+  } else {
+    
+    dat <- group_by(dat, dow, date, type, sp_abb, age, effort) %>% 
+      summarise(
+        ctch = length(tl_mm)
+      )
+      
+  }
+
+  # get CPUE as weight (or count) divided by effort
+  # sum by gear
+  # remove species other than carp, bullhead
+  dat <- ungroup(dat) %>% 
+    mutate(cpue = as.numeric(ctch)/as.numeric(effort)) %>% 
     group_by(dow, date, sp_abb, age) %>% 
     summarise(cpue = sum(cpue)) %>% 
     ungroup %>% 
     unite(var, sp_abb, age, sep = '_') %>% 
-    spread(var, cpue, fill = 0)
-  
-  names(dat)[names(dat) %in% 'other_adult'] <- 'other'
+    spread(var, cpue, fill = 0) %>% 
+    select(-other_adult)
   
   return(dat)
     
